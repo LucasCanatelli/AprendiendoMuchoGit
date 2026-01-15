eb9ec47917740592d7f27d5803058dcb26849b54
LucasCanatelli <luccangom4@alu.edu.gva.es>
2026-01-15 12:25:17 +0100

    Creado MensajeOperaciones.java

---
+++ MensajeOperaciones.java
@@ -0,0 +1,8 @@
+public class MensajeOperaciones {
+  public void mostrarMensajeInicio(){
+    System.out.println("Bienvenido al programa de operaciones!");
+  }
+  public void mostrarMensajeFin(){
+    System.out.println("Gracias por usar el programa de operaciones!");
+  }
+}
